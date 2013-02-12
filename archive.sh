8c61828accde31775029a4dd0f42b15948c62062
Thomas AUGUEY <contact@aceteam.org>
2013-02-12 18:18:28 +0100

    fix archive script commit count

--- archive.sh
+++ archive.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #exporte l'archive du projet avec son nom et son tag en cours
 #output="$(basename $(pwd))-$(git tag | sed -n 1p | tr -d '\n').zip"
-output="$(basename "$(pwd)")-$(git describe | sed -n 1p | tr -d '\n').zip"
+#output="$(basename "$(pwd)")-$(git describe | sed -n 1p | tr -d '\n').zip"
+output="$(basename "$(pwd)")-$(git tag | sed -n 1p | tr -d '\n')-$(git rev-list HEAD --count).zip"
+
 wd=$(pwd)
 #cd ..
 output="$(pwd)/../archives/$output"
