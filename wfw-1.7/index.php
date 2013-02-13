6cfafdf9e2d0437ef813b3b8b699fa14ac9e6022
Thomas AUGUEY <contact@aceteam.org>
2013-02-13 18:13:52 +0100

    update ui

--- wfw-1.7/index.php
+++ wfw-1.7/index.php
@@ -2,26 +2,7 @@
 require_once("inc/globals.php");
 global $app;
 
-//print_r($app);
-
-// charge une vue
-if(cInputFields::checkArray(array("page"=>"cInputName")))
-{
-    $param = array();
-    $app->showXMLView("view/user/pages/".$_REQUEST["page"].".html",$param);
-    exit;
-}
-
-$att = array(
-    "bdd_status"=>"Indisponible, vérifiez la configuration de l'application et l'installation de votre SGBD"
-);
-
-if($app->getDB($db_iface)){
-    $att["bdd_status"] = $db_iface->getServiceProviderName();
-    $att["bdd_status"] .= " ( ".$app->getCfgValue("database", "name")." @ ".$app->getCfgValue("database", "server")." : ".$app->getCfgValue("database", "port")." )";
-}
-
 // accueil
-$app->showXMLView("view/user/pages/index.html",$att);
+$app->showXMLView("view/user/pages/index.html",array());
 
 ?>
--- wfw-1.7/js/init.js
+++ wfw-1.7/js/init.js
@@ -13,59 +13,71 @@ YUI(wfw_yui_config(wfw_yui_base_path)).use('node', 'event', function (Y)
         
         //initialise l'interface
         var statusPanel = Ext.create('Ext.Panel', {
-                header :false,
-                //title: 'Status',
-                region: 'north',     // position for region
-                split: true,         // enable resizing
-                margins: '0 5 5 5',
+            header:false,
+            layout: 'hbox',
+            region: 'north',     // position for region
+            split: true,         // enable resizing
+            margins: '0 5 5 5',
+            /*html: Y.Node.one("#menu").get("innerHTML")*/
+            items: [{
+                header:false,
+                border: false,
+                width:200,
                 html: Y.Node.one("#header").get("innerHTML")
-            });
+            },{
+                header:false,
+                width:"100%",
+                border: false,
+                html: Y.Node.one("#status").get("innerHTML")
+            }],
+            renderTo: Ext.getBody()
+        });
             
         var contentPanel = Ext.create('Ext.Panel', {
-                header :false,
-                //title: 'Content',
-                region: 'center',     // position for region
-                height: 100,
-                split: true,         // enable resizing
-                margins: '0 5 5 5',
-                html: Y.Node.one("#content").get("innerHTML")
-            });
+            header :false,
+            //title: 'Content',
+            region: 'center',     // position for region
+            height: 100,
+            split: true,         // enable resizing
+            margins: '0 5 5 5',
+            html: Y.Node.one("#content").get("innerHTML")
+        });
             
         var menuPanel = Ext.create('Ext.Panel', {
-                title: 'Menu',
-                layout: {
-                    // layout-specific configs go here
-                    type: 'accordion',
-                    titleCollapse: false,
-                    animate: true,
-                    activeOnTop: true
-                },
-                region: 'west',     // position for region
-                width: 200,
-                split: true,         // enable resizing
-                margins: '0 5 5 5',
-                /*html: Y.Node.one("#menu").get("innerHTML")*/
-                items: [{
-                    title: 'Administrateur',
-                    html: Y.Node.one("#menu1").get("innerHTML")
-                },{
-                    title: 'Visiteur',
-                    html: Y.Node.one("#menu2").get("innerHTML")
-                },{
-                    title: 'Utilisateur',
-                    html: Y.Node.one("#menu3").get("innerHTML")
-                }],
-                renderTo: Ext.getBody()
-            });
+            title: 'Menu',
+            layout: {
+                // layout-specific configs go here
+                type: 'accordion',
+                titleCollapse: false,
+                animate: true,
+                activeOnTop: true
+            },
+            region: 'west',     // position for region
+            width: 200,
+            split: true,         // enable resizing
+            margins: '0 5 5 5',
+            /*html: Y.Node.one("#menu").get("innerHTML")*/
+            items: [{
+                title: 'Administrateur',
+                html: Y.Node.one("#menu1").get("innerHTML")
+            },{
+                title: 'Visiteur',
+                html: Y.Node.one("#menu2").get("innerHTML")
+            },{
+                title: 'Utilisateur',
+                html: Y.Node.one("#menu3").get("innerHTML")
+            }],
+            renderTo: Ext.getBody()
+        });
             
         var footerPanel = Ext.create('Ext.Panel', {
-                header :false,
-                //title: 'Pied de page',
-                region: 'south',     // position for region
-                split: true,         // enable resizing
-                margins: '0 5 5 5',
-                html: Y.Node.one("#footer").get("innerHTML")
-            });
+            header :false,
+            //title: 'Pied de page',
+            region: 'south',     // position for region
+            split: true,         // enable resizing
+            margins: '0 5 5 5',
+            html: Y.Node.one("#footer").get("innerHTML")
+        });
             
         var viewport = Ext.create('Ext.Viewport', {
             layout: 'border',
