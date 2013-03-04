bd02a3e4a9507240ac4cbec4094c5365709efc40
Thomas AUGUEY <contact@aceteam.org>
2013-03-04 17:14:48 +0100

    add jeu_essai.sql

---
+++ sql/postgresql/jeu_essai.sql
@@ -0,0 +1,12 @@
+
+
+/*---------------------------------------------------------------------------
+ UTILISATEURS
+----------------------------------------------------------------------------*/
+
+/* Identité */
+INSERT INTO USER_IDENTITY VALUES(1, NULL, 'thomas', 'auguey', '1983/12/25' ,'M');
+
+/* Comptes */
+INSERT INTO USER_ACCOUNT VALUES('toto', 1, NULL, 'avaace@hotmail.fr', 'nikita');
+INSERT INTO USER_ACCOUNT VALUES('AceTeaM', 1, NULL, 'contact@aceteam.org', 'popo');
