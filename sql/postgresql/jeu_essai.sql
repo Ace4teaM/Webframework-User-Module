cbbe7a5df37a9f26e7a6073e4a79fd6708778911
Thomas AUGUEY <contact@aceteam.org>
2013-04-11 11:51:31 +0200

    update to small password in 'jeu_essai'

--- sql/postgresql/jeu_essai.sql
+++ sql/postgresql/jeu_essai.sql
@@ -9,4 +9,4 @@ INSERT INTO USER_IDENTITY VALUES(1, NULL, 'thomas', 'auguey', '1983/12/25' ,'M')
 
 /* Comptes */
 INSERT INTO USER_ACCOUNT VALUES('toto', 1, NULL, 'avaace@hotmail.fr', 'nikita');
-INSERT INTO USER_ACCOUNT VALUES('AceTeaM', 1, NULL, 'contact@aceteam.org', 'popo');
+INSERT INTO USER_ACCOUNT VALUES('AceTeaM', 1, NULL, 'contact@aceteam.org', 'popodelasuper');
