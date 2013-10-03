4335b258d6bb9663e4dfc06d13224871c88224a9
Thomas AUGUEY <contact@aceteam.org>
2013-10-03 09:43:23 +0200

    add admin account to sample SQL data

--- sql/postgresql/jeu_essai.sql
+++ sql/postgresql/jeu_essai.sql
@@ -10,3 +10,4 @@ INSERT INTO USER_IDENTITY VALUES(1, NULL, 'thomas', 'auguey', '1983/12/25' ,'M')
 /* Comptes */
 INSERT INTO USER_ACCOUNT VALUES('toto', 1, NULL, 'avaace@hotmail.fr', 'nikita');
 INSERT INTO USER_ACCOUNT VALUES('AceTeaM', 1, NULL, 'contact@aceteam.org', 'popodelasuper');
+INSERT INTO USER_ACCOUNT VALUES('admin', NULL, NULL, 'dev@aceteam.org', 'root50110');
