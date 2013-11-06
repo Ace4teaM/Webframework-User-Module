912873433ecc50977981e344a1944ac00146433f
Thomas AUGUEY <contact@aceteam.org>
2013-11-06 09:55:15 +0100

    add list & set_identity controlers

---
+++ wfw-1.8/ctrl/user/list.php
@@ -0,0 +1,98 @@
+<?php
+/*
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    (C)2013 Thomas AUGUEY <contact@aceteam.org>
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    This file is part of WebFrameWork.
+
+    WebFrameWork is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebFrameWork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebFrameWork.  If not, see <http://www.gnu.org/licenses/>.
+    ---------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+/*
+ * Liste les comptes utilisateur
+ * Rôle : Administrateur
+ * UC   : adm_users
+ */
+
+class user_module_list_ctrl extends cApplicationCtrl
+{
+    public $fields    = null;
+    public $op_fields = null;
+
+    private $data = null; // XMLDocument
+   
+    function acceptedRole(){
+        return cApplication::AdminRole;
+    }
+
+    function __construct()
+    {
+        //ajoute les cookie aux données d'entrées
+        parent::__construct();
+        $this->att = array_merge($this->att,$_COOKIE);
+
+        //prepare la sortie
+        $this->data = new XMLDocument("1.0", "utf-8");
+        $this->data->appendChild($this->data->createElement('data'));
+    }
+    
+    function main(iApplication $app, $app_path, $p)
+    {
+        $doc = $this->data;
+
+        $query = "
+            select a.user_account_id, i.birth_day, i.sex, i.last_name, i.first_name, a.user_mail, c.user_session_id from user_account a
+                    full join user_identity i on i.user_identity_id = a.user_identity_id
+                    full join user_connection c on c.user_account_id = a.user_account_id
+                    where a.user_account_id!='admin'
+";
+
+        //obtient la bdd
+        if(!$app->getDB($db))
+            return false;
+        
+        if(!$db->execute($query, $result))
+            return false;
+        
+        //extrait les instances
+        while( $row = $result->fetchRow() ){
+            if(!is_array($row))
+                return RESULT(cResult::Failed, iDatabaseQuery::EmptyResult);
+
+            $node = $doc->createElement('user');
+            $doc->documentElement->appendChild($node);
+        
+            foreach($row as $id=>$value)
+                $node->appendChild( $doc->createtextElement($id,$value) );
+        }
+        
+        return RESULT_OK();
+    }
+    
+    function output(iApplication $app, $format, $att, $result)
+    {
+        if(!$result->isOK())
+            return parent::output($app, $format, $att, $result);
+
+        switch($format){
+            case "text/xml":
+                $doc = $this->data;
+                return '<?xml version="1.0" encoding="UTF-8" ?>'.$doc->saveXML( $doc->documentElement );
+        }
+        return parent::output($app, $format, $att, $result);
+    }
+};
+
+?>
---
+++ wfw-1.8/ctrl/user/set_identity.php
@@ -0,0 +1,53 @@
+<?php
+/*
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    (C)2012-2013 Thomas AUGUEY <contact@aceteam.org>
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    This file is part of WebFrameWork.
+
+    WebFrameWork is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebFrameWork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebFrameWork.  If not, see <http://www.gnu.org/licenses/>.
+    ---------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+/*
+ * Renseigne l'identité d'un utilisateur
+ * Crée ou modifie l'identité (nom, prénom, sex, ...) lié à un compte utilisateur
+ * 
+ * Rôle : Administrateur
+ * UC   : adm_identity
+ */
+
+class user_module_set_identity_ctrl extends cApplicationCtrl{
+    public $fields    = array('user_account_id', 'last_name', 'first_name', 'birth_day', 'sex');
+    public $op_fields = null;
+
+    function acceptedRole(){
+        return cApplication::AdminRole;
+    }
+
+    function __construct() {
+        parent::__construct();
+        $this->att = array_merge($this->att,$_COOKIE);
+    }
+    
+    function main(iApplication $app, $app_path, $p) {
+        //1. Actualise les données de l'identité
+        if(!UserModule::makeIdentity($p->user_account_id, $p->first_name, $p->last_name, $p->birth_day, $p->sex))
+            return false;
+
+        return true;//UserModule::makeIdentity
+    }
+};
+
+?>
